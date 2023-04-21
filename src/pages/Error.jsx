c7a8a8ad6a6079e4477d304deac065ee83501005
Sumer Patel <patelsum@oregonstate.edu>
2023-04-20 18:19:11 -0700

    error handling for form

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,27 @@
+// react-router-dom imports
+import { Link, useNavigate, useRouteError } from 'react-router-dom';
+
+// library imports
+import { HomeIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/solid';
+
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
+  
   return (
-    <div>
-      Error
+    <div className="error">
+      <h1>Uh Oh! We've got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn" onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon width={20} />
+          <span>Back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Home</span>
+        </Link>
+      </div>
     </div>
   )
 }
